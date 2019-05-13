804ac85118854631b53b6b86151bd50f709ec010
Dmitriy Tihonchik <dimatihiy1@gmail.com>
2019-05-13 13:12:57 +0300

    set up database settings

---
+++ src/main/resources/data-postgres.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.users(
+	login, admin, superadmin, password, email)
+	VALUES ("dima tihiy", true, true, "$2a$10$VH30dRRvt1BqtZDzitbiMu.6M/0VrDUsEqfrNU7eCYQnnsbV24/6y", "dimatihiy@ukr.net");
---
+++ src/main/resources/schema-postgres.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users(
+id serial PRIMARY KEY NOT NULL,
+login VARCHAR(100) UNIQUE,
+admin BOOLEAN NOT NULL,
+superadmin BOOLEAN NOT NULL,
+password VARCHAR(100),
+email VARCHAR(100) UNIQUE);
+
+DROP TABLE IF EXISTS events;
+
+CREATE TABLE events(
+id serial PRIMARY KEY NOT NULL,
+userid INT NOT NULL,
+description VARCHAR(200),
+eventdate TIMESTAMP NOT NULL,
+activeevent BOOLEAN NOT NULL,
+reminderexpression VARCHAR(100));
