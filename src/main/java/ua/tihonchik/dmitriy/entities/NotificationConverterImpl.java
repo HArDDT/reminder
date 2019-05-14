2dd070a05e043ea5733a717997e657e51cc4269e
Dmitriy Tihonchik <dimatihiy1@gmail.com>
2019-05-14 23:27:05 +0300

    was created notification converter

--- src/main/java/ua/tihonchik/dmitriy/entities/NotificationConverterImpl.java
+++ src/main/java/ua/tihonchik/dmitriy/entities/NotificationConverterImpl.java
@@ -1,56 +1,86 @@
 package ua.tihonchik.dmitriy.entities;
 
 import org.springframework.scheduling.support.CronSequenceGenerator;
+import ua.tihonchik.dmitriy.interfaces.NotificationConverter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
 
-//3week - 3 days before the date of the event, every week repeat
-//12year - 12 days before the date of the event, every year repeat
-//2day - 2 days before the date of the event, every day repeat
+public class NotificationConverterImpl implements NotificationConverter {
 
-public class NotificationConverterImpl {
+    private String expression = "";
+    private LocalDateTime eventDate = LocalDateTime.now();
 
-    private String expression;
-    private LocalDateTime eventDate;
+    /**
+     * NotificationConverterImpl - return the next valid date of notification
+     *
+     * @param expression - the template should have the number of days before the event date and the frequency of recurrence (day, week, month, year).
+     *                   Like this: "3week", "0, year", "2 day", "1 month".
+     * @param eventDate  - real date of event
+     *                   {@link NotificationConverterImpl#getNextValidDate(String, LocalDateTime)}
+     */
+    @Override
+    public LocalDate getNextValidDate(String expression, LocalDateTime eventDate) {
 
-    public NotificationConverterImpl(String expression, LocalDateTime eventDate) {
         this.expression = expression.toLowerCase();
         this.eventDate = eventDate;
-    }
-
-    public LocalDate getNextValidDate() {
 
         String cronExpression = getCronExpression();
         CronSequenceGenerator generator = new CronSequenceGenerator(cronExpression);
-        Date nextRunDate = generator.next(new Date());
+        LocalDate startDayForGenerator = getStartDayForGenerator();
+        Date firstNotificationDate = Date.from(startDayForGenerator.atStartOfDay(ZoneId.systemDefault()).toInstant());
+        Date nextRunDate = generator.next(firstNotificationDate);
         return nextRunDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
 
     }
 
+    private LocalDate getStartDayForGenerator() {
+        LocalDate notificationDate = getTheFirstNotificationDate();
+        LocalDate notificationDateExcluded = notificationDate.minusDays(1);
+        if (LocalDate.now().isAfter(notificationDateExcluded)) {
+            return LocalDate.now();
+        }
+        return notificationDateExcluded;
+    }
+
     private String getCronExpression() {
 
-        String duration = expression.replaceAll("(\\d|\\W)","");
+        String duration = getStringDuration();
+        LocalDate firstNotificationDate = getTheFirstNotificationDate();
+        String dayOfWeek = firstNotificationDate.getDayOfWeek().toString().substring(0, 3);
+        String month = firstNotificationDate.getMonth().toString().substring(0, 3);
+        int dayOfMonth = firstNotificationDate.getDayOfMonth();
         String regex = "";
         switch (duration) {
+            case "day":
+                regex = "0 0 0 * * ?";
+                break;
             case "week":
-                regex = "Color is Red";
+                regex = "0 0 0 * * " + dayOfWeek;
+                break;
+            case "month":
+                regex = "0 0 0 " + dayOfMonth + " * ?";
                 break;
             case "year":
-                regex = "Color is Green";
-                break;
-            case "day":
-                regex = "Color is Green";
+                regex = "0 0 0 " + dayOfMonth + " " + month + " ?";
         }
 
         return regex;
     }
 
-    private LocalDate getTheFirstNotificationDate(){
-        Integer countOfDays = Integer.getInteger(expression.replaceAll("(\\D|\\W)",""));
+    private LocalDate getTheFirstNotificationDate() {
+        int countOfDays = getCountDayBeforeEvent();
         return eventDate.minusDays(countOfDays).toLocalDate();
+    }
+
+    private String getStringDuration() {
+        return expression.replaceAll("(\\d|\\W)", "");
+    }
+
+    private int getCountDayBeforeEvent() {
+        return Integer.parseInt(expression.replaceAll("(\\D|\\W)", ""));
     }
 
 }
--- src/main/java/ua/tihonchik/dmitriy/interfaces/NotificationConverter.java
+++ src/main/java/ua/tihonchik/dmitriy/interfaces/NotificationConverter.java
@@ -1,4 +1,10 @@
 package ua.tihonchik.dmitriy.interfaces;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public interface NotificationConverter {
+
+    LocalDate getNextValidDate(String expression, LocalDateTime eventDate);
+
 }
