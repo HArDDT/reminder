2b36c757ad624d2ffe3354aa188746bb2945fa7f
Dmitriy Tihonchik <dimatihiy1@gmail.com>
2019-06-06 15:59:24 +0300

    security 2

--- src/main/java/ua/tihonchik/dmitriy/security/RestAuthenticationEntryPoint.java
+++ src/main/java/ua/tihonchik/dmitriy/security/RestAuthenticationEntryPoint.java
@@ -1,4 +1,4 @@
-package ua.tihonchik.dmitriy.additional;
+package ua.tihonchik.dmitriy.security;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
